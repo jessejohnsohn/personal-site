7dd300496014165fae52026821b298426783d231
Jesse Johnson <jesserjohnson@gmail.com>
2016-05-04 19:40:43 -0700

    fix it

---
+++ db/migrate/20160502214408_create_visitors.rb
@@ -0,0 +1,12 @@
+class CreateVisitors < ActiveRecord::Migration
+  def change
+    create_table :visitors do |t|
+      t.string :name
+      t.string :email
+      t.string :title
+      t.string :phone
+
+      t.timestamps null: false
+    end
+  end
+end
